392ad905b2456ad63228a9f528dbc59d494487dd
youssefkead <105119228+youssefkead@users.noreply.github.com>
2022-05-18 11:35:51 -0400

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,12 +1,7 @@
-
-var select = document.getElementById("select-tool");
 function getSelectedValue()
 {
     var selectedValue = document.getElementById("select-tool").value;
     var btn = document.getElementById("selected-link")
 
-btn.setAttribute("href", selectedValue)
-
-    console.log(selectedValue);
+    btn.setAttribute("href", selectedValue)
 }
-getSelectedValue();
